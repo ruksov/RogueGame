35c2b222632b467ae7f77c2f889a51e5935483b6
funky_coder <e.ruksov@gmail.com>
2023-08-10 16:02:55 +0300

    Add dummy Room Node Graph Editor window

---
+++ Assets/Scripts/NodeGraph/Editor/RoomNodeGraphEditor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Rogue.NodeGraph.Editor
+{
+  public class RoomNodeGraphEditor : EditorWindow
+  {
+    [MenuItem("Room Node Graph Editor", menuItem = "Tools/Room Node Graph Editor")]
+    private static void OpenWindow()
+    {
+      GetWindow<RoomNodeGraphEditor>("Room Node Graph Editor");
+    }
+
+    private void OnGUI()
+    {
+      Debug.Log("OnGUI");
+    }
+  }
+}
